8294c78aeb2f676065b8b0c59457e085604b9f5a
mario-galindo <mgalindo9410@gmail.com>
2021-09-13 18:29:32 -0600

    Replace domain entities with data transfer objects

--- SocialMedia.API/Controllers/PostController.cs
+++ SocialMedia.API/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
+using System.Linq;
 using SocialMedia.Core.Interfaces;
 
 namespace SocialMedia.API.Controllers
@@ -19,19 +21,42 @@ namespace SocialMedia.API.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var postsDto = posts.Select(x => new PostDto
+            {
+                PostId = x.PostId,
+                Date = x.Date,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId
+            });
+            return Ok(postsDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+            var postDto = new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+            };
+            return Ok(postDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post
+            {
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
